7eb57ce9b314dda0432a013280275d61d21a4212
EvgeniyOVorobev <vorobiev71@yandex.ru>
2023-05-05 16:26:57 +0300

    +refracting

--- src/main/java/ru/ev/SpringTelegramBot/service/TelegramBot.java
+++ src/main/java/ru/ev/SpringTelegramBot/service/TelegramBot.java
@@ -9,10 +9,15 @@ import org.springframework.stereotype.Component;
 import org.telegram.telegrambots.bots.TelegramLongPollingBot;
 import org.telegram.telegrambots.meta.api.methods.commands.SetMyCommands;
 import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
+import org.telegram.telegrambots.meta.api.methods.updatingmessages.EditMessageText;
 import org.telegram.telegrambots.meta.api.objects.Message;
 import org.telegram.telegrambots.meta.api.objects.Update;
 import org.telegram.telegrambots.meta.api.objects.commands.BotCommand;
 import org.telegram.telegrambots.meta.api.objects.commands.scope.BotCommandScopeDefault;
+import org.telegram.telegrambots.meta.api.objects.replykeyboard.InlineKeyboardMarkup;
+import org.telegram.telegrambots.meta.api.objects.replykeyboard.ReplyKeyboardMarkup;
+import org.telegram.telegrambots.meta.api.objects.replykeyboard.buttons.InlineKeyboardButton;
+import org.telegram.telegrambots.meta.api.objects.replykeyboard.buttons.KeyboardRow;
 import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
 import ru.ev.SpringTelegramBot.config.BotConfig;
 import ru.ev.SpringTelegramBot.entity.User;
@@ -34,6 +39,8 @@ public class TelegramBot extends TelegramLongPollingBot {
             "Type /mydata to see data stored about yourself\n\n"+
             "Type /help to see this message again";
 
+    static final String YES_BUTTON="YES_BUTTON";
+    static final String NO_BUTTON="NO_BUTTON";
     public TelegramBot(BotConfig botConfig) {
         this.botConfig = botConfig;
         List<BotCommand> list = new ArrayList<>();
@@ -57,21 +64,97 @@ public class TelegramBot extends TelegramLongPollingBot {
 
             long chatId = update.getMessage().getChatId();
 
+            if(messageText.contains("/send")&&botConfig.getOwnerId()==chatId){
+                var textToSend=EmojiParser.parseToUnicode(messageText.substring(messageText.indexOf(" ")));
+                var users= userRepository.findAll();
+                for (User user:users){
+                    prepareMessage(user.getChadId(), textToSend);
+                }
+            }
 
-            switch (messageText) {
-                case "/start":
-                    startCommandReceived(chatId, update.getMessage().getChat().getFirstName());
-                    registerUser(update.getMessage());
-                    break;
-                case "/help":
-                    sendMessage(chatId, HELP_TEXT);
-                    break;
-                default:
+            else {
+                switch (messageText) {
+                    case "/start":
+                        startCommandReceived(chatId, update.getMessage().getChat().getFirstName());
+                        registerUser(update.getMessage());
+                        break;
+                    case "/help":
+                        prepareMessage(chatId, HELP_TEXT);
+                        break;
 
-                    sendMessage(chatId, "Sorry");
+                    case "/register":
+                        register(chatId);
 
+
+                    default:
+
+                        prepareMessage(chatId, "Sorry");
+
+                }
             }
         }
+
+        else if(update.hasCallbackQuery()){
+            long messageId =update.getCallbackQuery().getMessage().getMessageId();
+            long chatId=update.getCallbackQuery().getMessage().getChatId();
+            String callBackData=update.getCallbackQuery().getData();
+            if(callBackData.equals(YES_BUTTON)){
+                String text="You pressed yes button";
+                executeEditMessageText(text,chatId,messageId);
+            }
+            else if(callBackData.equals(NO_BUTTON)){
+                String text="You pressed No button";
+                executeEditMessageText(text,chatId,messageId);
+            }
+        }
+    }
+
+    private void executeEditMessageText(String text, long chatId, long messageId) {
+
+        EditMessageText messageText=new EditMessageText();
+        messageText.setChatId(String.valueOf(chatId));
+        messageText.setText(text);
+        messageText.setMessageId((int) messageId);
+        try{
+            execute(messageText);
+        }catch (TelegramApiException e){
+            log.error("Error "+e.getMessage());
+        }
+    }
+
+    private void executeMessage(SendMessage message){
+        try{
+            execute(message);
+        }catch (TelegramApiException e){
+            log.error("Error "+e.getMessage());
+        }
+    }
+
+    private void register(long chatId) {
+        SendMessage message=new SendMessage();
+        message.setChatId(String.valueOf(chatId));
+        message.setText("Do you really want to register?");
+
+        InlineKeyboardMarkup markup=new InlineKeyboardMarkup();
+        List<List<InlineKeyboardButton>> rowsInLine=new ArrayList<>();
+        List<InlineKeyboardButton> rowInLine=new ArrayList<>();
+        var button=new InlineKeyboardButton();
+        button.setText("Yes");
+        button.setCallbackData(YES_BUTTON);
+
+        var button1=new InlineKeyboardButton();
+        button1.setText("No");
+        button1.setCallbackData(NO_BUTTON);
+
+        rowInLine.add(button);
+        rowInLine.add(button1);
+        rowsInLine.add(rowInLine);
+
+        markup.setKeyboard(rowsInLine);
+        message.setReplyMarkup(markup);
+
+        executeMessage(message);
+
     }
 
     private void registerUser(Message message) {
@@ -102,12 +185,35 @@ public class TelegramBot extends TelegramLongPollingBot {
         SendMessage message = new SendMessage();
         message.setChatId(String.valueOf(chatId));
         message.setText(textToSend);
-        try {
-            execute(message);
-        } catch (TelegramApiException e) {
-            log.error("Error occurred: " + e.getMessage());
-        }
 
+        ReplyKeyboardMarkup keyboardMarkup=new ReplyKeyboardMarkup();
+        List<KeyboardRow> keyboardRows=new ArrayList<>();
+        KeyboardRow row=new KeyboardRow();
+
+        row.add("weather");
+        row.add("get random joke");
+
+        keyboardRows.add(row);
+
+        KeyboardRow row1=new KeyboardRow();
+        row1.add("register");
+        row1.add("check my data");
+        row1.add("delete my data");
+        keyboardRows.add(row1);
+
+        keyboardMarkup.setKeyboard(keyboardRows);
+        message.setReplyMarkup(keyboardMarkup);
+        executeMessage(message);
+
+
+
+    }
+
+    private void prepareMessage(long chatId,String textToSend){
+        SendMessage message = new SendMessage();
+        message.setChatId(String.valueOf(chatId));
+        message.setText(textToSend);
+        executeMessage(message);
     }
 
 
